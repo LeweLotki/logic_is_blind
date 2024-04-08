6827a450879a006efc1ee49af9eeac7d63b49bc3
LeweLotki <stefanczyk@gmail.com>
2024-04-08 17:10:33 +0200

    app restart file

---
+++ .restart_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PID=$(ps -ef | grep 'logic-is-blind' | grep -v 'grep' | awk '{print $2}')
+PROJECT_HOME="/home/lewelotki/Desktop/logic_is_blind"
+
+if [ ! -z "$PID" ]; then
+  kill $PID
+  sleep 2
+fi
+
+cd $PROJECT_HOME;
+poetry run python run.py &
+
