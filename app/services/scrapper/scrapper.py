a1fed950fd30bb25422fad8ba1a76b9b09e5dfec
LeweLotki <stefanczyk@gmail.com>
2024-04-11 01:07:44 +0200

    succesful logic master scrapper

--- app/services/scrapper/scrapper.py
+++ app/services/scrapper/scrapper.py
@@ -1,12 +1,11 @@
-import re
 import requests
-from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
 class PuzzleScrapper:
     
     data = dict()
+    soup = None
 
     def __init__(self): pass 
 
@@ -20,25 +19,40 @@ class PuzzleScrapper:
         print(self.data)
 
     def __scrape_logic_master(self, url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
 
-        puzzle_image = soup.find('img')['src']
-        difficulty_row = soup.find('td', string=re.compile('Difficulty'))#.parent# if soup.find('td', string='Difficulty:') else None
-        print(f'difficulty row: {difficulty_row}')
+        response = requests.get(url)
+        self.soup = BeautifulSoup(response.text, 'html.parser')
+
+        return {
+            'puzzle_image'   : self.__get_puzzle_image(),
+            'sudoku_pad_ref' : self.__get_sudoku_pad_ref(),
+            'difficulty'     : self.__get_difficulty(),
+            'solution_code'  : self.__get_solution_code() 
+        }
+
+    def __get_puzzle_image(self) -> str():
+
+        return self.soup.find('img')['src']
+
+    def __get_sudoku_pad_ref(self) -> str():
+
+        sudoku_pad_ref = None
+        sudoku_pad_ref_tag = self.soup.find('a', href=lambda href: href and 'crackingthecryptic' in href)
+        if sudoku_pad_ref_tag:
+            sudoku_pad_ref = sudoku_pad_ref_tag['href']
+        return sudoku_pad_ref
+
+    def __get_difficulty(self) -> str():
+
+        difficulty_row = self.soup.find(lambda tag: tag.name == 'td' and 'Schwierigkeit' in tag.get_text())
         difficulty = None
         if difficulty_row:
             difficulty = difficulty_row.find('img').get('title')
+        return difficulty
 
-        # sudoku_pad_ref = soup.find('a', href=True)['href']
-        solution_code = soup.find('span', class_='rp_loesungscode_descr').text
+    def __get_solution_code(self) -> str():
 
-        return {
-            'puzzle_image'   : puzzle_image,
-            # 'sudoku_pad_ref' : sudoku_pad_ref,
-            'difficulty'     : difficulty,
-            'solution_code'  : solution_code
-        }
+        return self.soup.find('span', class_='rp_loesungscode_descr').text
 
     def __scrape_sudokupad(self, url):
         response = requests.get(url)
