ce5bf815ea5d5cdd16322d3fd2404613996be2f4
LeweLotki <stefanczyk@gmail.com>
2024-04-28 13:02:13 +0200

    frontend init - adding footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+import FooterBar from './components/FooterBar';
+
+function App() {
+  return (
+    <div className="min-h-screen w-full bg-gray-300">
+
+      <FooterBar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/FooterBar/index.jsx
@@ -0,0 +1,30 @@
+import { FaGithub } from 'react-icons/fa';
+import { FaPatreon } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa';
+
+
+const FooterBar = () => {
+    return (
+      <div className="fixed bottom-0 left-0 w-full h-16 flex justify-center items-center bg-gray-600 text-white shadow-lg">
+        <p>© 2024 Logic Is Blind</p>
+        <Divider />
+        <FooterBarIcon icon={<FaGithub size="28" />} href="https://github.com/LeweLotki" />
+        <Divider />
+        <FooterBarIcon icon={<FaPatreon size="28" />} href="https://www.patreon.com/"  />
+        <Divider />
+        <FooterBarIcon icon={<FaYoutube size="28" />} href="https://www.youtube.com/"  />
+        <Divider />
+      </div>
+    );
+  };
+  
+  const FooterBarIcon = ({ icon, href='' }) => (
+    <a className="footerbar-icon group" href={href}>
+      {icon}
+    </a>
+  );
+
+const Divider = () => <hr className="sidebar-hr" />;
+
+export default FooterBar;
+  
