51a9ca886985848992dfccb47c3e3b668d94cb4a
LeweLotki <stefanczyk@gmail.com>
2024-04-28 22:29:18 +0200

    adding feature to component logo

--- frontend/src/components/Logo/index.jsx
+++ frontend/src/components/Logo/index.jsx
@@ -1,9 +1,12 @@
-import logoImage from '../../lib_logo.jpg';
+import React from 'react';
+import logoBW from '../../lib_logo.jpg'; // Adjust the path as needed
+import logoColor from '../../lib_logo_color.jpg'; // Path to the colored logo
 
 const Logo = () => {
   return (
-    <div className="fixed top-4 left-4 hover:scale-110 transition-transform duration-200">
-      <img src={logoImage} alt="Logo" className="h-50 w-50 rounded-full" />
+    <div className="logo-container fixed top-4 left-4 h-50 w-50">
+      <img src={logoBW} alt="Logo" className="logo-bw absolute inset-0 w-full h-full object-cover rounded-full" />
+      <img src={logoColor} alt="Color Logo" className="logo-color absolute inset-0 w-full h-full object-cover rounded-full opacity-0 transition-opacity duration-200" />
     </div>
   );
 };
