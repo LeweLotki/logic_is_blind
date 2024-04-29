94d4944929b7b4687fe7f872555b93aafd4bf054
LeweLotki <stefanczyk@gmail.com>
2024-04-29 22:53:52 +0200

    changing page title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import './index.css';
 
+import { Helmet } from 'react-helmet';
+
 import FooterBar from './components/FooterBar';
 import Logo from './components/Logo';
 import Sidebar from './components/SideBar';
 import PuzzleList from './components/PuzzleList';
 import Filter from './components/Filter';
+
 import { FilterProvider } from './hooks/FilterContext';
+
+const TITLE = 'Logic Is Blind';
 
 function App() {
   return (
     <div className="app-container min-h-screen w-full bg-gray-300">
-
+        <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
       <Logo />
       <Sidebar />
       <FilterProvider >
