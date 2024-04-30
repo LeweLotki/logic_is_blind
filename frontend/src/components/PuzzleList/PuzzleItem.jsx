ef7cbf65ffed101bf12a02db0619c26efe7a5617
LeweLotki <stefanczyk@gmail.com>
2024-04-30 15:40:22 +0200

    adding state with puzzle for Preview page

--- frontend/src/components/PuzzleList/PuzzleItem.jsx
+++ frontend/src/components/PuzzleList/PuzzleItem.jsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 const PuzzleItem = ({ puzzle }) => {
+
+  const navigate = useNavigate(); // Hook to get navigate function
+
+  // Function to handle navigation
+  const handleNavigate = () => {
+    navigate('/preview', { state: { puzzle } }); // Adjust this path as needed for your routing setup
+  };
+
   return (
     <div className="puzzle-item-container relative">
-      <button className="puzzle-item flex justify-center items-center py-3 px-6 text-white font-montserrat w-full">
+      <button onClick={handleNavigate} className="puzzle-item flex justify-center items-center py-3 px-6 text-white font-montserrat w-full">
         <span className="puzzle-label">{puzzle.title}</span>
         <span className="divider"></span>
         <span className="puzzle-label">{puzzle.author}</span>
--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -1,15 +1,20 @@
+import { useLocation } from 'react-router-dom';
+
 import FooterBar from '../components/FooterBar';
 import Logo from '../components/Logo';
 import Sidebar from '../components/SideBar';
 
 
 function App() {
+
+  const location = useLocation();
+  const { puzzle } = location.state || {};
+
   return (
     <div className="app-container min-h-screen w-full bg-gray-300">
       <Logo />
       <Sidebar />
       <FooterBar />
-
     </div>
   );
 }
